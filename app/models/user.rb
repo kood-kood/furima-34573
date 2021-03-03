91fa11734f19204bed4777ec28f8dcf79a210f0f
kood-kood <koujikozi@icloud.com>
2021-03-03 15:32:00 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email,              presence: true
+  validates :encrypted_password, presence: true
+  validates :nickname,           presence: true
+  validates :birthday,           presence: true
+  validates :last_name,          presence: true
+  validates :given_names,        presence: true
+  validates :last_name_furigana, presence: true
+  validates :given_names_furigana, presence: true
 end
