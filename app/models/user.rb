b52dadcaabfcda281f1626a32c1567a7f61ab73a
kood-kood <koujikozi@icloud.com>
2021-03-03 17:17:20 +0900

    名前、パスワード、生年月日　実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,20 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, password_length: 6..128
 
-  validates :email,              presence: true
-  validates :encrypted_password, presence: true
+  # validates :email,              presence: true
+  # validates :encrypted_password, presence: true
   validates :nickname,           presence: true
   validates :birthday,           presence: true
   validates :last_name,          presence: true
   validates :given_names,        presence: true
   validates :last_name_furigana, presence: true
   validates :given_names_furigana, presence: true
+
+  with_options presence: true do
+  validates :encrypted_password,:password,:password_confirmation,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
+  end
 end
+
+# validates :encrypted_password,:password,:password_confirmation,length:{minimum:7},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
