b6151dd31a7b2f9b158721ed0e3016ade7b37031
kood-kood <koujikozi@icloud.com>
2021-03-03 20:06:12 +0900

    ユーザー本名入力フォーム　調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,8 @@ class User < ApplicationRecord
 
   with_options presence: true do
   validates :encrypted_password,:password,:password_confirmation,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
-  validates :last_name,:given_names,:last_name_furigana,:given_names_furigana,format:{with: /[ぁ-んァ-ヶ一-龥々]/}
+  validates :last_name,:given_names,format:{with: /[ぁ-んァ-ヶ一-龥々]/}
+  validates :last_name_furigana,:given_names_furigana,format:{with: /[ァ-ヶー]/}
   end
 end
 
