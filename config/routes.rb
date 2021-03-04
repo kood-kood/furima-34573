10d9822d8bd641afb6fe6f1acbceed978b5e0ecc
kood-kood <koujikozi@icloud.com>
2021-03-04 18:49:30 +0900

    Revert "出品機能"
    This reverts commit 03eae1976cd79316e48efafba4d8b13a07b0d8d7.

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'items/index'
   root to: 'items#index'
-
   # root 'items#index'
   resources :items, only: :index
-
-  resources :items, only: [:index, :new, :edit, :show] do
-    resources :orders, only: :index
-  end
-
-  # resources :users, only: :show
-
 end
