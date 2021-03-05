49c99dea83eace21333e1fd3f353b737f4820d2e
kood-kood <koujikozi@icloud.com>
2021-03-05 11:14:55 +0900

    ユーザー管理機能　３回目の再登録

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,27 +1,10 @@
 class ItemsController < ApplicationController
-  # before_action :move_to_index, except: [:index, :show]
+
 
   def index
-    # @items = Item.all
+    @items = Item.all
   end
 
-  # def new
-  # end
-
-  # def edit
-  # end
-
-  # def show
-  # end
-
-
-  # private
-
-  # def move_to_index
-  #   unless user_signed_in?
-  #     redirect_to action: :index
-  #   end
-  # end
 
  def new
   end
