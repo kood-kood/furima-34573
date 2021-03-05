bff9f5e6b175a68d790cef039dec4c1dae83da0d
kood-kood <koujikozi@icloud.com>
2021-03-05 10:46:25 +0900

    ユーザー管理機能　2回目の再登録

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
 
-    resources :orders, only: :index
   resources :items, only: [:index, :new, :edit, :show]
   #  do
   #   resources :orders, only: :index
