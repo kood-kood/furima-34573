5a884e15351ac693843d5fc6c28f5813e26659ce
kood-kood <koujikozi@icloud.com>
2021-03-05 11:25:37 +0900

    Revert "ユーザー管理機能　2回目の再登録"
    This reverts commit bff9f5e6b175a68d790cef039dec4c1dae83da0d.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
 
+    resources :orders, only: :index
   resources :items, only: [:index, :new, :edit, :show]
   #  do
   #   resources :orders, only: :index
