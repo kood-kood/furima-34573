0ab081ef361d5b60e7d338337a7026f074384717
kood-kood <koujikozi@icloud.com>
2021-03-05 16:29:17 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
 
   belongs_to :user 
+  belongs_to :listing
   has_one_attached :image    #imageカラムの保存を許可
-
+  
 end
---
+++ db/migrate/20210305062606_create_listings.rb
@@ -0,0 +1,13 @@
+class CreateListings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :listings do |t|
+      t.string :category           , null: false
+      t.string :product_condition  , null: false
+      t.string :shipping_charge    , null: false
+      t.string :shipping_area      , null: false
+      t.date   :days_to_ship       , null: false
+      t.integer :user_id           , null: false
+      t.timestamps
+    end
+  end
+end
