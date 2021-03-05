b9507be99d774eeecb646569d6351a3096276597
kood-kood <koujikozi@icloud.com>
2021-03-05 14:50:29 +0900

    Active Storageのコードを記入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,12 +15,16 @@ class ItemsController < ApplicationController
   # end
 
 
-  # private
+  private
 
   # def move_to_index
   #   unless user_signed_in?
   #     redirect_to action: :index
   #   end
   # end
+
+  def item_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+
+  belongs_to :user 
+  has_one_attached :image    #imageカラムの保存を許可
+
 end
