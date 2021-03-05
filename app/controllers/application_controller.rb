76df59419374d03685b292a815298dcd292bfd56
kood-kood <koujikozi@icloud.com>
2021-03-05 12:05:54 +0900

    仮１

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,23 +5,17 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birthday, :last_name, :given_names, :last_name_furigana, :given_names_furigana])
+    devise_parameter_sanitizer.permit(:sign_in,keys:[:email, :password])
+  end
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
     end
   end
-<<<<<<< HEAD
-=======
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-  end
-  
->>>>>>> master
 end
 
 
 
-
-  
-  
