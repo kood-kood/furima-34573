8f84373f32b11c8f6aa4da2bfca9ec4fd53aedf4
kood-kood <koujikozi@icloud.com>
2021-03-05 14:08:56 +0900

    マイグレーションを編集

--- db/migrate/20210305012421_create_items.rb
+++ db/migrate/20210305012421_create_items.rb
@@ -1,8 +1,17 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string     :product_name
+      t.integer    :price
+      t.text       :description
+      t.integer    :product_condition
+      t.integer    :shipping_charge
+      t.integer    :shipping_area
+      t.integer    :days_to_ship
+      t.integer    :category
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
+
 end
