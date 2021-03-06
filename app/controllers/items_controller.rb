e00c7df06d1955c5a155934f4110f8c3dfab8d52
kood-kood <koujikozi@icloud.com>
2021-03-06 21:24:17 +0900

    出品一覧を新規から表示する様に調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    # @user_item = UserItem.new
-    @items = Item.all
+    @items = Item.all.order(created_at: :desc)
   end
 
   def new
