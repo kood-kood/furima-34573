363a804555d50c1ccb3bbd520b4bc4f60d9bb207
kood-kood <koujikozi@icloud.com>
2021-03-07 11:25:43 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :new, :show]
+  before_action :move_to_index, except: [:index, :show]
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
