f60e0ed1d06883c40c8b0277e2d0ae87ac73ef2a
kood-kood <koujikozi@icloud.com>
2021-03-08 17:05:47 +0900

    コントローラーを設定　2回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   # before_action :move_to_index, except: [:index, :show]
-  before_action :set_item, only: [:edit, :update, :show]
+  before_action :set_item, only: [:edit, :update, :show, :destroy]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -37,8 +37,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @tweet = Tweet.find(params[:id])
-    @tweet.destroy
+    if @item.destroy
+      redirect_to root_path
+    end
   end
 
 
