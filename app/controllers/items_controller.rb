85df424f4f6ef9e74934db8a926d573a22a79371
kood-kood <koujikozi@icloud.com>
2021-03-08 12:49:16 +0900

    コントローラーのupdateアクションを調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,14 +13,14 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  # def update
-  #   item = Item.find(params[:id])
-  #   if item.update(item_params)
-  #     redirect_to item_path
-  #   else
-  #     render :edit
-  #   end
-  # end
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
 
   def show
     @item = Item.find(params[:id])
