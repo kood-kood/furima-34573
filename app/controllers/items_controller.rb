4b0fbb00dd9b4c85ff3fbafddc3cb00a323746be
kood-kood <koujikozi@icloud.com>
2021-03-08 15:17:53 +0900

    「private」内を調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,14 +41,10 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:content,:image, :product_name, :price, :description, :category_id, :product_condition_id,:shipping_charge_id, :shipping_area_id, :days_to_ship_id).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    redirect_to new_user_session_path unless user_signed_in?
-  end
-
   def set_item
     @item = Item.find(params[:id])
    end
-   
+
   # def article_params
   #   params.require(:item).permit(:user_id)
   # end
