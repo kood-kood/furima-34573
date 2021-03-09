d6a333673ed80487feb45831cadc7ec9b67124d4
kood-kood <koujikozi@icloud.com>
2021-03-09 16:24:41 +0900

    購入画面を表示できる様に調整

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,22 +1,23 @@
 class OrdersController < ApplicationController
   def index
     @order = Order.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
-    # @order = Order.new(order_params)
-    # if @order.valid?
-    #   @order.save
-    #   return redirect_to root_path
-    # else
-    #   render 'index'
-    # end
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
 
   private
 
-  # def order_params
-  #   params.require(:order).permit(:price)
-  # end
+  def order_params
+    params.require(:order).permit(:address, :phone_number, :postal_code, :shipping_area_id, :municipality, :building_name)
+  end
 
 end
