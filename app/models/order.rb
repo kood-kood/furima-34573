4973369db555ad3f9aa38b5fab471d8459cdf194
kood-kood <koujikozi@icloud.com>
2021-03-09 20:27:38 +0900

    バリデーション設定と tokenの値もOrderモデルで取り扱えるように設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,18 +1,22 @@
-class Order < ApplicationRecord
-
-  # include ActiveModel::Model
-  # attr_accessor :hoge,:fuga...
+class Order 
+  include ActiveModel::Model
+  attr_accessor :token
 
   # ここにバリデーションの処理を書く
-  validates :user, presence: true
-  validates :item, presence: true
-  # def save
-    # 各テーブルにデータを保存する処理を書く
-  # end
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :postal_code, presence: true
+  validates :shipping_area_id, presence: true
+  validates :municipality, presence: true
+
+  validates :user_id, presence: true
+  validates :item_id, presence: true
+  def save
+    
+  end
 
 
-  belongs_to :user
-  belongs_to :item
+ 
   # has_one :domicile
 
 end
