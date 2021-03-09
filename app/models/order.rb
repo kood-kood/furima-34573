dd94b2944b67928a488efa01e7b8455925144dca
kood-kood <koujikozi@icloud.com>
2021-03-09 12:49:38 +0900

    index,createアクションを設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,16 @@
 class Order < ApplicationRecord
 
+  # include ActiveModel::Model
+  # attr_accessor :hoge,:fuga...
+
+  # ここにバリデーションの処理を書く
+  validates :user, presence: true
+  validates :item, presence: true
+  # def save
+    # 各テーブルにデータを保存する処理を書く
+  # end
+
+
   belongs_to :user
   belongs_to :item
   # has_one :domicile
