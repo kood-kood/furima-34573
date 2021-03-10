cc85930c33672e3fd4c976f9e742075507ca6557
kood-kood <koujikozi@icloud.com>
2021-03-10 21:22:32 +0900

    郵便番号と電話番号のバリデーションを設定

--- app/models/order_domicile.rb
+++ app/models/order_domicile.rb
@@ -14,18 +14,18 @@ class OrderDomicile
 
   # ここにバリデーションの処理を書く
   validates :token, presence: true
-
   validates :address, presence: true
-  validates :phone_number, presence: true
-  validates :postal_code, presence: true
+  validates :phone_number, presence: true, format: { with: /\A\d{11}\z/ }
+  validates :postal_code, presence: true, format: { with: /\A\d{3}[-]\d{4}\z/ }
   validates :shipping_area_id, presence: true
   validates :municipality, presence: true
-
   validates :user_id, presence: true
   validates :item_id, presence: true
 
-  def save
-   item = Item.create(user_id: user_id, item_id: item_id)
-    Address.create(address: address, phone_number: phone_number, postal_code: postal_code, shipping_area_id: shipping_area_id, municipality: municipality)
-  end
+  # def save
+
+  #  order = Order.create(user_id: user_id, item_id: item_id)
+
+  #   Domicile.create(address: address, phone_number: phone_number, postal_code: postal_code, shipping_area_id: shipping_area_id, municipality: municipality, order_id: order.id)
+  # end
 end
