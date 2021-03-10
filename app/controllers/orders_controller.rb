f92f54c174f1d3d229579c61b03b6feb2aadf74d
kood-kood <koujikozi@icloud.com>
2021-03-10 14:30:03 +0900

    秘密鍵代入した環境変数を呼び込む

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,14 @@
 class OrdersController < ApplicationController
   def index
-    @order = Order.new
+    @order = Order.new(order_params)
     @item = Item.find(params[:item_id])
   end
 
   def create
-    binding.pry
+    @item = Item.find(params[:item_id])
     @order = Order.new(order_params)
     if @order.valid?
-      Payjp.api_key = "sk_test_b84679cb4ec2c4debf6068a4"
-      Payjp::Charge.create(
-        amount: order_params[:price],
-        card: order_params[:token],
-        currency: 'jpy'
-      )
+      pay_item
       @order.save
       return redirect_to root_path
     else
@@ -26,5 +21,14 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order).permit(:address, :phone_number, :postal_code, :shipping_area_id, :municipality, :building_name).merge(user_id: params[:user_id], item_id: params[:item_id], token: params[:token])
   end
-  
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+  Payjp::Charge.create(
+    amount: order_params[:price],
+    card: order_params[:token],
+    currency: 'jpy'
+  )
+  end
+
 end
