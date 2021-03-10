5dfe56b9747b47e75f00ff9a93e4b744256ce092
kood-kood <koujikozi@icloud.com>
2021-03-10 16:58:18 +0900

    各モデルの「default: ""」を消去

--- db/migrate/20210310071337_create_domiciles.rb
+++ db/migrate/20210310071337_create_domiciles.rb
@@ -1,7 +1,12 @@
 class CreateDomiciles < ActiveRecord::Migration[6.0]
   def change
     create_table :domiciles do |t|
-
+      t.string     :address,                 null: false, default: ""
+      t.string     :phone_number,            null: false, default: ""
+      t.string     :postal_code,             null: false, default: ""
+      t.integer    :shipping_area_id,        null: false
+      t.string     :municipality,            null: false, default: ""
+      t.string     :building_name
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_091806) do
+ActiveRecord::Schema.define(version: 2021_03_10_071337) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,24 +33,39 @@ ActiveRecord::Schema.define(version: 2021_03_08_091806) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "domiciles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "address", default: "", null: false
+    t.string "phone_number", default: "", null: false
+    t.string "postal_code", default: "", null: false
+    t.integer "shipping_area_id", null: false
+    t.string "municipality", default: "", null: false
+    t.string "building_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "product_name"
-    t.integer "price"
-    t.text "description"
-    t.integer "product_condition_id"
-    t.integer "shipping_charge_id"
-    t.integer "shipping_area_id"
-    t.integer "days_to_ship_id"
-    t.integer "category_id"
-    t.bigint "user_id"
+    t.string "product_name", default: "", null: false
+    t.integer "price", null: false
+    t.text "description", null: false
+    t.integer "product_condition_id", null: false
+    t.integer "shipping_charge_id", null: false
+    t.integer "shipping_area_id", null: false
+    t.integer "days_to_ship_id", null: false
+    t.integer "category_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -73,4 +88,6 @@ ActiveRecord::Schema.define(version: 2021_03_08_091806) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "orders", "items"
+  add_foreign_key "orders", "users"
 end
