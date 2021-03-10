21b2eb69bd851913633b45da4a28240d072767cb
kood-kood <koujikozi@icloud.com>
2021-03-10 12:23:14 +0900

    バリデーションを調整

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,16 +1,20 @@
 class Order 
   include ActiveModel::Model
+
+    # ここにバリデーションの処理を書く
   attr_accessor :token
+  attr_accessor :address
+  attr_accessor :phone_number
+  attr_accessor :postal_code
+  attr_accessor :shipping_area_id
+  attr_accessor :municipality
+  attr_accessor :building_name
 
-  # ここにバリデーションの処理を書く
-  validates :address, presence: true
-  validates :phone_number, presence: true
-  validates :postal_code, presence: true
-  validates :shipping_area_id, presence: true
-  validates :municipality, presence: true
+  attr_accessor :user_id
+  attr_accessor :item_id
+  
+  # validates :token, presence: true
 
-  validates :user_id, presence: true
-  validates :item_id, presence: true
   def save
     
   end
