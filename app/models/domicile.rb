ff8eeb53f7c8c25c7b8671ef28acad2b61f43fb2
kood-kood <koujikozi@icloud.com>
2021-03-10 17:45:31 +0900

    「order_domicile.rb」を作成

--- app/models/domicile.rb
+++ app/models/domicile.rb
@@ -1,10 +1,5 @@
 class Domicile < ApplicationRecord
 
-  validates :address_id, presence: true
-  validates :phone_number_id, presence: true
-  validates :postal_code_id, presence: true
-  validates :shipping_area_id, presence: true
-  validates :municipality_id, presence: true
-
   belongs_to :order
+  
 end
---
+++ app/models/order_domicile.rb
@@ -0,0 +1,31 @@
+class OrderDomicile
+  include ActiveModel::Model
+  attr_accessor :token
+
+  attr_accessor :address
+  attr_accessor :phone_number
+  attr_accessor :postal_code
+  attr_accessor :shipping_area_id
+  attr_accessor :municipality
+  attr_accessor :building_name
+
+  attr_accessor :user_id
+  attr_accessor :item_id
+
+  # ここにバリデーションの処理を書く
+  validates :token, presence: true
+
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :postal_code, presence: true
+  validates :shipping_area_id, presence: true
+  validates :municipality, presence: true
+
+  validates :user_id, presence: true
+  validates :item_id, presence: true
+
+  def save
+   item = Item.create(user_id: user_id, item_id: item_id, token: token)
+    Address.create(address: address, phone_number: phone_number, postal_code: postal_code, shipping_area_id: shipping_area_id, municipality: municipality)
+  end
+end
