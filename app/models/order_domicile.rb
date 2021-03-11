4c7158428a6fbd6c8d0646da4b757323781746c0
kood-kood <koujikozi@icloud.com>
2021-03-11 11:51:05 +0900

    attr_accessorをまとめた

--- app/models/order_domicile.rb
+++ app/models/order_domicile.rb
@@ -1,26 +1,18 @@
 class OrderDomicile
   include ActiveModel::Model
-  attr_accessor :token
 
-  attr_accessor :address
-  attr_accessor :phone_number
-  attr_accessor :postal_code
-  attr_accessor :shipping_area_id
-  attr_accessor :municipality
-  attr_accessor :building_name
-
-  attr_accessor :user_id
-  attr_accessor :item_id
+  attr_accessor :token, :address, :phone_number, :postal_code, :shipping_area_id, :municipality, :building_name, :user_id, :item_id
 
   # ここにバリデーションの処理を書く
   validates :token, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true, format: { with: /\A\d{11}\z/ }
   validates :postal_code, presence: true, format: { with: /\A\d{3}[-]\d{4}\z/ }
-  validates :shipping_area_id, presence: true
   validates :municipality, presence: true
   validates :user_id, presence: true
   validates :item_id, presence: true
+
+  # validates :shipping_area_id, numericality: { other_than: 1 }
 
   def save
    order = Order.create(user_id: user_id, item_id: item_id)
