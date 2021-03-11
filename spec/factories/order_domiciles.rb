c43bcef14aec1e9e9935609b70d8ec5e6c48dea1
kood-kood <koujikozi@icloud.com>
2021-03-11 14:33:29 +0900

    単体テストコード完成

---
+++ spec/factories/order_domiciles.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_domicile do
+    token            {"tok_abcdefghijk00000000000000000"}
+    postal_code      {"123-1234"}
+    shipping_area_id { 2 }
+    municipality     {"横浜市緑区"}
+    address          {"青山1-1-1"}
+    phone_number     {"08012345678"}
+  end
+end
---
+++ spec/models/order_domicile_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+RSpec.describe OrderDomicile, type: :model do
+  before do
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item)
+    @order = FactoryBot.build(:order_domicile, item_id: @item.id, user_id: @user.id)
+    sleep 0.1
+  end
+
+  describe '商品購入機能' do
+    context '商品が購入できるとき' do
+      it '全ての項目の入力が存在すれば購入できること' do
+        expect(@order).to be_valid
+      end
+    end
+
+    context '商品が購入できない時' do
+      it "tokenが空では登録できないこと" do
+        @order.token = nil
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Tokenが入力されていません。")
+      end
+      it 'postal_codeが空では登録できないこと' do
+        @order.postal_code = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Postal codeが入力されていません。", "Postal codeは有効でありません。")
+      end
+      it 'shipping_area_idが空では登録できないこと' do
+        @order.shipping_area_id = 1
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Shipping areaは1以外の値にしてください")
+      end
+      it 'municipalityが空では登録できないこと' do
+        @order.municipality = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Municipalityが入力されていません。")
+      end
+      it 'addressが空では登録できないこと' do
+        @order.address = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Addressが入力されていません。")
+      end
+      it 'phone_numberが空では登録できないこと' do
+        @order.phone_number = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Phone numberが入力されていません。", "Phone numberは有効でありません。")
+      end
+     
+      it '郵便番号にハイフンが含まれていなければ登録できないこと' do
+        @user.email = '1234567'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("メールアドレスは有効でありません。")
+      end
+      it '電話番号は12桁以上では登録できないこと' do
+        @order.phone_number = '0901234567891'
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Phone numberは11文字以下に設定して下さい。")
+      end
+            
+    end
+
+  end
+
+end
