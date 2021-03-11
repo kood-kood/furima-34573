c8185db6bbcbf465699d9f60c0c6d6a31e655ccd
kood-kood <koujikozi@icloud.com>
2021-03-11 17:06:09 +0900

    建物番号のカラムをFactoryBotに追加

--- spec/factories/order_domiciles.rb
+++ spec/factories/order_domiciles.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     shipping_area_id { 2 }
     municipality     {"横浜市緑区"}
     address          {"青山1-1-1"}
+    building_name    {"柳ビル103"}
     phone_number     {"08012345678"}
   end
 end
