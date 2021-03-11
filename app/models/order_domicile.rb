76ce886f31cefbf40b70fc8f21a925ff38b3d35d
kood-kood <koujikozi@icloud.com>
2021-03-11 11:54:11 +0900

    「presence: true」をまとめた

--- app/models/order_domicile.rb
+++ app/models/order_domicile.rb
@@ -4,15 +4,17 @@ class OrderDomicile
   attr_accessor :token, :address, :phone_number, :postal_code, :shipping_area_id, :municipality, :building_name, :user_id, :item_id
 
   # ここにバリデーションの処理を書く
-  validates :token, presence: true
-  validates :address, presence: true
-  validates :phone_number, presence: true, format: { with: /\A\d{11}\z/ }
-  validates :postal_code, presence: true, format: { with: /\A\d{3}[-]\d{4}\z/ }
-  validates :municipality, presence: true
-  validates :user_id, presence: true
-  validates :item_id, presence: true
+  with_options presence: true do
+   validates :token
+   validates :address
+   validates :phone_number, format: { with: /\A\d{11}\z/ }
+   validates :postal_code,  format: { with: /\A\d{3}[-]\d{4}\z/ }
+   validates :municipality
+   validates :user_id
+   validates :item_id
+  end
 
-  validates :shipping_area_id, numericality: { other_than: 1 }
+   validates :shipping_area_id, numericality: { other_than: 1 }
 
   def save
    order = Order.create(user_id: user_id, item_id: item_id)
