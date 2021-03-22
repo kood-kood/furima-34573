16b2cd3eaa80afa03954dbffbbddc48d3bbf6288
kood-kood <koujikozi@icloud.com>
2021-03-22 21:14:29 +0900

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock "~> 3.16.0"
 set :application, "furima-34573"
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url, "git@example.com:kood-kood/furima-34573.git"
+set :repo_url, "git@github.com:kood-kood/furima-34573.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
