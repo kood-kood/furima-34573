2e26cec69ffb524b224179bea91db45cba78cacd
kood-kood <koujikozi@icloud.com>
2021-03-23 16:58:21 +0900

    memoのコントローラーにauthenticate_user!を設置

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,2 +1,20 @@
 class MemosController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :new, :create]
+
+  def index
+    @memos = Memo.all.order(created_at: :desc)
+  end
+
+   def new
+    @memos = Memo.all
+    @memo = Memo.new
+  end
+
+  def create
+    @memo = Memo.new(memo_params)
+    # if @memo.save
+    #   ActionCable.server.broadcast 'memo_channel', content: @memo
+    # end
+  end
+
 end
